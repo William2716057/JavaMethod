d2663a6516ce2c23080a014fcf42c8d2bb68ef5d
William <willspri@gmail.com>
2024-02-24 13:41:36 +1300

    file added

---
+++ Method.java
@@ -0,0 +1,11 @@
+public class Method {
+	public static void main(String[] args) {
+		int result = add(9,7);
+		System.out.println("Result: " + result);
+	}
+
+public static int add(int a, int b) {
+	return a + b;
+}
+
+}
